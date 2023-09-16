515de00480c792991a7cc3e66118c70e60fb1e38
Miguel Martins <miguel.vale.martins@gmail.com>
2023-09-16 12:51:02 +0100

    Add event listners to the buttons

--- javascript.js
+++ javascript.js
@@ -1,12 +1,50 @@
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+
+let display = document.querySelector("#display");
+
+const numbers = document.querySelectorAll(".num");
+numbers.forEach(button => {
+    button.addEventListener("click", () => {
+        operator ? secondNumber += button.textContent : firstNumber += button.textContent;
+        updateDisplay();
+    });    
+});
+
+const operators = document.querySelectorAll(".operator");
+operators.forEach(button => {
+    button.addEventListener("click", () => {
+        operator = button.textContent;
+        updateDisplay();
+    });    
+});
+
+const equals = document.querySelector("#equals");
+equals.addEventListener("click", () => operate(+firstNumber, +secondNumber, operator));
+
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", () => {firstNumber = ""; secondNumber = ""; operator = ""; updateDisplay()});
+
 function operate(x, y, operator){
     switch(operator){
         case "+":
-            return x + y;
+            display.textContent = x + y;
+            firstNumber = display.textContent;
+            break;
         case "-":
-            return x - y;
+            display.textContent = x - y;
+            firstNumber = display.textContent;
+            break;
         case "*":
-            return x * y;
+            display.textContent = x * y;
+            firstNumber = display.textContent;
+            break;
         case "/":
-            return x / y;
+            display.textContent = x / y;
+            firstNumber = display.textContent;
+            break;
+        default:
+            alert("Invalid format!");
     }
 }
